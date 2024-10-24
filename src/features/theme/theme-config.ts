6d669fe00b81b6b7f30c7dfc8325740ae5aaac03
Scott Vande Krol <scva@softchoice.com>
2024-10-24 11:11:27 -0700

    Changed name of application to III ChatGPT

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "III ChatGPT";
+export const AI_DESCRIPTION = "III ChatGPT is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
